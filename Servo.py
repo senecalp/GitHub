df70cb8482b0b2b930452ec43177e8a77410c5b3
Seneca Pressley <senecalp@mac.com>
2024-12-24 15:56:40 -0500

    set Sensor pin, remove half rotation

--- Servo.py
+++ Servo.py
@@ -7,13 +7,16 @@ servo_pin = machine.Pin(15)
 servo = PWM(servo_pin)
 
 # Set Duty Cycle for Different Angles
-max_duty = 7500
+max_duty = 10000
 min_duty = 1
-half_duty = 4200
 
 #Set PWM frequency
 frequency = 50
 servo.freq (frequency)
+
+# Set up sensor Pin
+sensor_pin = 14
+sensor = Pin(sensor_pin, Pin.IN)
 
 try:
     while True:
@@ -21,9 +24,7 @@ try:
             #Servo at 0 degrees
             servo.duty_u16(min_duty)
             sleep(2)
-            #Servo at 90 degrees
-            servo.duty_u16(half_duty)
-            sleep(2)
+
             #Servo at 180 degrees
             servo.duty_u16(max_duty)
             sleep(2)    
